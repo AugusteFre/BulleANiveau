f1535c2af60e73bafa45751517c98a6f844059c8
Auguste Fresard <auguste.fresard@divtec.ch>
2023-05-15 09:58:00 +0200

    Test accéleromètre

--- app/src/main/java/com/example/bulleaniveau/MainActivity.java
+++ app/src/main/java/com/example/bulleaniveau/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.bulleaniveau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor gyroscope;
+    private TextView gyroscopeValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gyroscopeValues = findViewById(R.id.gyroscopeValues);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            // Mettez à jour votre TextView avec les valeurs du gyroscope
+            gyroscopeValues.setText("X: " + x + "\nY: " + y + "\nZ: " + z);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Ne faites rien pour l'instant
     }
 }
