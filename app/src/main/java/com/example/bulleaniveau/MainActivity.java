efebb528b9f6758b599ac3accddb3a3a09b3d3ad
Auguste Fresard <auguste.fresard@divtec.ch>
2023-05-15 10:51:27 +0200

    Test positionnement

--- app/src/main/java/com/example/bulleaniveau/MainActivity.java
+++ app/src/main/java/com/example/bulleaniveau/MainActivity.java
@@ -7,7 +7,10 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -15,16 +18,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor gyroscope;
     private TextView gyroscopeValues;
 
+    private int screenHeight;
+    private int screenWidth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHeight = displayMetrics.heightPixels;
+        screenWidth = displayMetrics.widthPixels;
 
         gyroscopeValues = findViewById(R.id.gyroscopeValues);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
+
 
     @Override
     protected void onResume() {
@@ -40,18 +53,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             float x = event.values[0];
             float y = event.values[1];
             float z = event.values[2];
 
             // Mettez à jour votre TextView avec les valeurs du gyroscope
             gyroscopeValues.setText("X: " + x + "\nY: " + y + "\nZ: " + z);
+            // Appelez une méthode pour mettre à jour la position du TextView
+            updateTextViewPosition(x, y);
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Ne faites rien pour l'instant
+    }
+
+    private void updateTextViewPosition(float x, float y) {
+
+        // Calculez les nouvelles coordonnées en ajoutant les valeurs du gyroscope
+        float newX = (x * (screenWidth/2f/10f) + screenWidth/2f);
+        float newY = (y * (screenHeight/2f/10f) + screenHeight/2f);
+
+        // Définissez les nouvelles coordonnées pour le TextView
+        gyroscopeValues.setX(newX);
+        gyroscopeValues.setY(newY);
     }
 }
